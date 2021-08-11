019a719b074b0c0b519e48301a0f933019bc2cc8
Nam Luu <nam.luu@isobar.com>
2021-08-12 00:16:32 +0700

    admin - layout

---
+++ admin/menu.php
@@ -0,0 +1,5 @@
+<a href="./">Home</a>
+<a href="./list-section.php">Section</a>
+<a href="./list-category.php">Category</a>
+<a href="./list-post.php">Post</a>
+<a href="./list-banner.php">Banner</a>
